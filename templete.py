53dc5cb19d7c027ce40cd0f5919b0c8bcff83454
Abhinav <abhinavyadavv3012@gmail.com>
2026-05-23 21:48:33 +0530

    Merge branch 'main' of https://github.com/Aabhiinavvv/end2endmlproject

--- templete.py
+++ templete.py
@@ -1,1 +1,11 @@
 import os 
+import sys
+import time 
+def main():
+    print("Hello World")
+    time.sleep(1)
+    print("Goodbye World")
+if __name__ == "__main__":
+    main()  
+
+                
